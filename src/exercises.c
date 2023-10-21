5c7980f2fbc53b84a5265a9660b4954592666341
Your Name <mahiqureshii77@gmail.com.unl.edu>
2023-10-22 01:23:08 +0500

    lab4

--- src/exercises.c
+++ src/exercises.c
@@ -1,64 +1,92 @@
-/**
- * Author(s):
- * Date:
- *
- * This is a series of exercises to introduce loop
- * control structures.
- *
- */
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
 int main(int argc, char **argv) {
-
   if(argc != 2) {
     fprintf(stderr, "Usage: %s n\n", argv[0]);
     exit(1);
   }
 
-  int n = atoi(argv[1]);
+  int n = atoi(argv[1);
 
-  // A simple for loop that prints 0 thru 9
-  for(int i=0; i<10; i++) {
-    printf("%d\n", i);
-  }
-
-  //1. Rewrite the given for loop as an equivalent while loop
-  for(int i=10; i<20; i+=3) {
+  // 1. Rewrite the given for loop as an equivalent while loop
+  int i = 10;
+  while(i < 20) {
     printf("%d ", i);
+    i += 3;
   }
   printf("\n");
 
-  //2. Rewrite the given while loop as an equivalent for loop
-  int k = 3;
-  while(k > -4) {
+  // 2. Rewrite the given while loop as an equivalent for loop
+  for(int k = 3; k > -4; k--) {
     printf("%d ", k);
-    k--;
   }
   printf("\n");
 
-  //3. Print numbers 1 thru n separated by a comma except for
-  //   the last one; example: 1, 2, 3, 4, 5
+  // 3. Print numbers 1 thru n separated by a comma except for the last one
+  for(int i = 1; i <= n; i++) {
+    printf("%d", i);
+    if(i < n) {
+      printf(", ");
+    }
+  }
+  printf("\n");
 
-  //4. Print squares of numbers 1 thru n separated by a comma
-  //   except for the last one; example: 1, 4, 9, 16, 25
+  // 4. Print squares of numbers 1 thru n separated by a comma
+  for(int i = 1; i <= n; i++) {
+    printf("%d", i * i);
+    if(i < n) {
+      printf(", ");
+    }
+  }
+  printf("\n");
 
-  //5. Compute the summation of numbers 1 thru n and print
-  //   the result; example: 1 + 2 + 3 + 4 + 5 = 15
+  // 5. Compute the summation of numbers 1 thru n and print the result
+  int sum = 0;
+  for(int i = 1; i <= n; i++) {
+    sum += i;
+    printf("%d", i);
+    if(i < n) {
+      printf(" + ");
+    }
+  }
+  printf(" = %d\n", sum);
 
-  //6. Compute the summation of squares of numbers 1 thru n
-  //   and print the result; example: 1^2 + 2^2 + 3^2 + 4^2 + 5^2 = 55
+  // 6. Compute the summation of squares of numbers 1 thru n and print the result
+  int sumSquares = 0;
+  for(int i = 1; i <= n; i++) {
+    sumSquares += i * i;
+    printf("%d^2", i);
+    if(i < n) {
+      printf(" + ");
+    }
+  }
+  printf(" = %d\n", sumSquares);
 
-  //7. Compute the summation of even numbers 1 thru n and print
-  //   the result; example (n=5): 2 + 4 = 6
+  // 7. Compute the summation of even numbers 1 thru n and print the result
+  int sumEven = 0;
+  for(int i = 2; i <= n; i += 2) {
+    sumEven += i;
+    printf("%d", i);
+    if(i < n) {
+      printf(" + ");
+    }
+  }
+  printf(" = %d\n", sumEven);
 
-  //8. Write a solution to the following variation of the FizzBuzz
-  //   problem.  Print out integers 1 thru n, one to a line, except
-  //   that if the integer is divisible by 7 print "Foo" instead.
-  //   If the integer is divisible by 11 print "Bar" instead.  If
-  //   the integer is divisible by both 7 and 11, print "FooBar"
-  //   instead.
+  // 8. Print "Foo," "Bar," or "FooBar" based on the divisibility of the number
+  for(int i = 1; i <= n; i++) {
+    if (i % 7 == 0 && i % 11 == 0) {
+      printf("FooBar\n");
+    } else if (i % 7 == 0) {
+      printf("Foo\n");
+    } else if (i % 11 == 0) {
+      printf("Bar\n");
+    } else {
+      printf("%d\n", i);
+    }
+  }
 
   return 0;
 }
